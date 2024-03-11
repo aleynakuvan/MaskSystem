036a84aecbf4bdd94fcf32461b234b61e1177b53
aleynakuvan <114821414+aleynakuvan@users.noreply.github.com>
2024-03-11 13:58:23 +0300

    Add files via upload

---
+++ Works/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using Entities.Concrete;
+
+
+namespace Works
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            SelamVer();
+
+            
+
+
+            Person person1 = new Person();
+            Console.WriteLine("Adınızı giriniz:");
+            person1.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Soyadınızı giriniz:");
+            person1.LastName = Console.ReadLine();
+            Console.WriteLine("Doğum tarihinizi giriniz:" );
+            person1.DateOfBirthYear = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Tc numarasını giriniz:" );
+
+
+            person1.NationalIdentity = Convert.ToInt64(Console.ReadLine());
+           
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+
+        }
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba!!!");
+        }
+    }
+
+    
+}
